112a4f7428e901cce1937489afeba6c61886ccce
Jacob Hands <jacob@gogit.io>
2017-09-18 14:42:44 -0500

    Add Set-AllowedHosts

--- JHUtils.psm1
+++ JHUtils.psm1
@@ -1,4 +1,4 @@
-# Generated on: 09/18/2017 14:17:59
+# Generated on: 09/18/2017 14:42:24
 # Description: Helpful PowerShell Modules
 # Author: Jacob Hands <jacob@gogit.io>
 
@@ -109,5 +109,44 @@ Function Test-Platform {
         }
     }
 }
+Function Set-AllowedHosts {
+    <#
+        .SYNOPSIS
+        Checks to see if something is allowed to run on the current PC.
+        
+        .DESCRIPTION
+        Checks to see if something is allowed to run on the current PC.
+        If not, the script is stopped with -ErrorAction:Stop
+        
+        .PARAMETER Computers
+        List of computer names allowed.
 
+        .PARAMETER Windows
+        Only allow script on Windows
+
+        .PARAMETER Unix
+        Only allow script on Unix
+        
+        .EXAMPLE
+        Set-AllowedHosts ENVY10
+        
+        .NOTES
+        General notes
+    #>
+    Param (
+        [string[]]$Computers,
+        [switch]$Windows,
+        [switch]$Unix
+    )
+    if ($Computers -and -not $Computers.Contains($env:COMPUTERNAME)) {
+        Write-Error "Computer: $($env:COMPUTERNAME) is not on list of valid computers: $($names -join ', ')" -ErrorAction:Stop
+    }
+    if(-not ($Windows -and $Unix)){
+        if($Windows -and -not (Test-Platform -Windows)){
+            Write-Error "Platform: $(Get-Platform) not allowed! Must be Windows." -ErrorAction:Stop
+        }elseif($Unix -and -not (Test-Platform -Unix)){
+            Write-Error "Platform: $(Get-Platform) not allowed! Must be Unix." -ErrorAction:Stop
+        }
+    }
+}
 Export-ModuleMember -Function Get-Platform,Test-Platform
--- Modules/MultiPlatformUtils.psm1
+++ Modules/MultiPlatformUtils.psm1
@@ -56,5 +56,44 @@ Function Test-Platform {
         }
     }
 }
+Function Set-AllowedHosts {
+    <#
+        .SYNOPSIS
+        Checks to see if something is allowed to run on the current PC.
+        
+        .DESCRIPTION
+        Checks to see if something is allowed to run on the current PC.
+        If not, the script is stopped with -ErrorAction:Stop
+        
+        .PARAMETER Computers
+        List of computer names allowed.
 
-Export-ModuleMember -Function Get-Platform,Test-Platform
+        .PARAMETER Windows
+        Only allow script on Windows
+
+        .PARAMETER Unix
+        Only allow script on Unix
+        
+        .EXAMPLE
+        Set-AllowedHosts ENVY10
+        
+        .NOTES
+        General notes
+    #>
+    Param (
+        [string[]]$Computers,
+        [switch]$Windows,
+        [switch]$Unix
+    )
+    if ($Computers -and -not $Computers.Contains($env:COMPUTERNAME)) {
+        Write-Error "Computer: $($env:COMPUTERNAME) is not on list of valid computers: $($names -join ', ')" -ErrorAction:Stop
+    }
+    if(-not ($Windows -and $Unix)){
+        if($Windows -and -not (Test-Platform -Windows)){
+            Write-Error "Platform: $(Get-Platform) not allowed! Must be Windows." -ErrorAction:Stop
+        }elseif($Unix -and -not (Test-Platform -Unix)){
+            Write-Error "Platform: $(Get-Platform) not allowed! Must be Unix." -ErrorAction:Stop
+        }
+    }
+}
+Export-ModuleMember -Function Get-Platform,Test-Platform,Set-AllowedHosts
