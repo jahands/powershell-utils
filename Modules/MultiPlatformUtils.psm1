45e923ef212cfb44ac661883db918a7c61df9bf9
Jacob Hands <jacob@gogit.io>
2017-09-25 08:26:32 -0500

    Check if platform is specified

--- Modules/MultiPlatformUtils.psm1
+++ Modules/MultiPlatformUtils.psm1
@@ -129,10 +129,13 @@ Function Set-AllowedHosts {
     if ($Computers -ne $null -and -not $Computers.Contains($env:COMPUTERNAME)) {
         Write-Error "Computer: $($env:COMPUTERNAME) is not on list of valid computers: $($Computers -join ', ')" -ErrorAction:Stop
     }
-    if ($IsLinux -and -not ($Linux -or $Unix)) {
-        Write-Error 'This command cannot be run on Linux!' -ErrorAction:Stop
-    } elseif ($IsWindows -and -not $Windows) {
-        Write-Error 'This command cannot be run on Windows!' -ErrorAction:Stop
+    # Check if platform is specified
+    if($Windows -or $Unix -or $Linux){
+        if ($IsLinux -and -not ($Linux -or $Unix)) {
+            Write-Error 'This command cannot be run on Linux!' -ErrorAction:Stop
+        } elseif ($IsWindows -and -not $Windows) {
+            Write-Error 'This command cannot be run on Windows!' -ErrorAction:Stop
+        }
     }
 }
 
