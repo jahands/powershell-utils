09fe59394c949349014c902b3b4fee9de46c451d
Jacob Hands <jacob@gogit.io>
2017-10-02 11:52:56 -0500

    Add Test-Image

--- JHUtils.psm1
+++ JHUtils.psm1
@@ -1,4 +1,4 @@
-# Generated on: 09/25/2017 08:26:40
+# Generated on: 10/02/2017 11:50:18
 # Description: Helpful PowerShell Modules
 # Author: Jacob Hands <jacob@gogit.io>
 
@@ -885,3 +885,52 @@ Function New-DPDictionary {
     Return New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
 }
 Export-ModuleMember -Function New-DynamicParameter, New-DPDictionary
+
+# ============================================ #
+
+# Test-Image.psm1
+# Found this function here: https://blogs.technet.microsoft.com/heyscriptingguy/2015/03/19/psimaging-part-1-test-image/
+Function Test-Image {
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param(
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('PSPath')]
+        [string] $Path
+    )
+    PROCESS {
+        $knownHeaders = @{
+            jpg  = @( "FF", "D8" );
+            jpeg = @( "FF", "D8" );
+            bmp  = @( "42", "4D" );
+            gif  = @( "47", "49", "46" );
+            tif  = @( "49", "49", "2A" );
+            png  = @( "89", "50", "4E", "47", "0D", "0A", "1A", "0A" );
+            pdf  = @( "25", "50", "44", "46" );
+        }
+        # coerce relative paths from the pipeline into full paths
+        if ($_ -ne $null) {
+            $Path = $_.FullName
+        }
+        # read in the first 8 bits
+        $bytes = Get-Content -LiteralPath $Path -Encoding Byte -ReadCount 1 -TotalCount 8 -ErrorAction Ignore
+        $retval = $false
+        foreach ($key in $knownHeaders.Keys) {
+            # make the file header data the same length and format as the known header
+            $fileHeader = $bytes |
+            Select-Object -First $knownHeaders[$key].Length |
+            ForEach-Object { $_.ToString("X2") }
+            if ($fileHeader.Length -eq 0) {
+                continue
+            }
+            # compare the two headers
+            $diff = Compare-Object -ReferenceObject $knownHeaders[$key] -DifferenceObject $fileHeader
+            if (($diff | Measure-Object).Count -eq 0) {
+                $retval = $true
+            }
+        }
+        return $retval
+    }
+}
+Export-ModuleMember -Function Test-Image
